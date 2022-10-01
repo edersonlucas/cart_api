610afd3bf0cbf3f6578d9cb6a290b6f98619cda6
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-01 20:32:54 -0300

    feat: create model Cart

--- src/main/java/com/edersonlucas/carrinho/models/Cart.java
+++ src/main/java/com/edersonlucas/carrinho/models/Cart.java
@@ -1,4 +1,33 @@
 package com.edersonlucas.carrinho.models;
 
+import com.edersonlucas.carrinho.enumeration.FormOfPayment;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JsonIgnore
+    private Client client;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Item> items;
+    private Double totalValue;
+    @Enumerated
+    private FormOfPayment formOfPayment;
+    private boolean closed;
 }

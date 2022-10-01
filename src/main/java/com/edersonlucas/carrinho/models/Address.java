fc97595332a5373a2fcecb8516d4f2ff5312cc9c
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-01 20:33:26 -0300

    feat: create model Address

--- src/main/java/com/edersonlucas/carrinho/models/Address.java
+++ src/main/java/com/edersonlucas/carrinho/models/Address.java
@@ -1,4 +1,19 @@
 package com.edersonlucas.carrinho.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Embeddable
+@NoArgsConstructor
 public class Address {
+    private String zipCode;
+    private String complement;
 }

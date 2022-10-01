4af83f8f70c28bac98a9135671abfb7c4382f2bf
Ederson Lucas <edersonlucas@outlook.com.br>
2022-10-01 20:31:16 -0300

    feat: create model Restaurant

--- src/main/java/com/edersonlucas/carrinho/models/Restaurant.java
+++ src/main/java/com/edersonlucas/carrinho/models/Restaurant.java
@@ -1,4 +1,26 @@
 package com.edersonlucas.carrinho.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Product> menu;
+    @Embedded
+    private Address address;
 }
